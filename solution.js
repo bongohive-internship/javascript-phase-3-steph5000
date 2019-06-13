88bbb6f4934b88d2d9cc32aa8af347172690dfda
steph5000 <51511634+steph5000@users.noreply.github.com>
2019-06-13 13:49:25 +0200

    javascript phase3

--- solution.js
+++ solution.js
@@ -10,5 +10,18 @@ for (number=0; number<15; number++) {
   var number;
   var total = 0;
   for (number=1; number<1000; number++) { if ((number % 3 === 0) || (number % 5 === 0)) {
-    total = total + number;
+    total = total + number; }
+                                        }
+ // forth solution
+  function assigngrade(score) {
+    var grade;
+    if (score >= 9) { grade = "A"; }
+    else if (score>= 80) { grade = "B"; }
+    else if ( score>=70) { grade = "C"; }
+    else if (score>=60) { grade ="D"; }
+    else if (score>=59) { grade= "E"; }
+    else { grade = "F"; }
+    return grade; }
+  
+  
     
