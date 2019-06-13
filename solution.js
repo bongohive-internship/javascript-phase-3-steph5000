05c2f9764f2b73199e7ee5544d607fac4da83ec3
steph5000 <51511634+steph5000@users.noreply.github.com>
2019-06-13 13:51:32 +0200

    phase 4 solutions

--- solution.js
+++ solution.js
@@ -1,14 +1,14 @@
 // solutions to the exercises mentioned in the readme should be in this file.
 function largernum(a, b) {
   return Math.max(a, b) }
-// second program.
+// second solution
 var number;
 for (number=0; number<15; number++) {
   if (number % 2 == 0) { console.log(number + "" + "is even"); }
   else { console.log(number + "" + "is old"); }
-  //third  program.
+  //third  solution
   var number;
-  var total = 0;
+  var total = 0; }
   for (number=1; number<1000; number++) { if ((number % 3 === 0) || (number % 5 === 0)) {
     total = total + number; }
                                         }
