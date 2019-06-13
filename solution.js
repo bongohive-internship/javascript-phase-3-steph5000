d8405288108072b5a3913ad4d6bd9c7537aaeee5
steph5000 <51511634+steph5000@users.noreply.github.com>
2019-06-13 13:34:20 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -4,7 +4,11 @@ function largernum(a, b) {
 // second program.
 var number;
 for (number=0; number<15; number++) {
-  if (number % 2 == 0) { console.log("number is even"); }
-  else { console.log("number is old"); }
+  if (number % 2 == 0) { console.log(number + "" + "is even"); }
+  else { console.log(number + "" + "is old"); }
   //third  program.
-  
+  var number;
+  var total = 0;
+  for (number=1; number<1000; number++) { if ((number % 3 === 0) || (number % 5 === 0)) {
+    total = total + number;
+    
